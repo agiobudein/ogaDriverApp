0b40c9c69c950c858b8f5268ef29753f10fd25bf
agiobudein <agiobudein@gmail.com>
2024-01-22 11:35:29 +0100

    set up

---
+++ src/main/java/com/tamjayz/ogaDriverApp/OgaDriverAppApplication.java
@@ -0,0 +1,13 @@
+package com.tamjayz.ogaDriverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgaDriverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgaDriverAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamjayz/ogaDriverApp/OgaDriverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamjayz.ogaDriverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgaDriverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
